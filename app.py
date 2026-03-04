00a36ef5ebb72c73f7291721ac91a0a14d26c3f3
Zardian18 <zardian018@gmail.com>
2026-03-04 00:54:54 +0000

    Added radio button for better visualization

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback, Input, Output
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
@@ -9,52 +9,64 @@ df = pd.read_csv("preprocessed_data.csv")
 df["date"] = pd.to_datetime(df["date"])
 df = df.sort_values('date')
 breakpoint_date = datetime(2021,1,15)
-fx = px.line(df, x = 'date', y = 'sales', color = 'region', title = 'Combined graph', labels={
-    'sales':'Sales', 'date':'Dates'
-})
-fx.add_vline(x = breakpoint_date, line_dash='dot', line_color='red', line_width = 2)
-fx.update_layout(
-        height = 400
-    )
-regions = df['region'].unique()
+
 color_dict = {'north':'pink', 'south':'yellow', 'east':'blue', 'west':'green'}
-
-figures = {}
-for r in regions:
-    region_df = df[df['region']==r]
-    fig = px.line(region_df, x = 'date', y = 'sales', title = f'{r.title()} region sales', labels={
-        'sales':'Sales', 'date':'Dates'
-    })
-    fig.update_traces(
-        line_color = color_dict[r]
-    )
-    fig.add_vline(x = breakpoint_date, line_dash='dot', line_color='red', line_width = 2)
-    fig.update_layout(
-        height = 400,
-        showlegend = False
-    )
-
-    figures[r] = fig
-
 
 app.layout = html.Div([
     html.H1("Sales trend by region", style={'textAlign':'center', 'marginBottom':'30px'}),
     html.Div([
-        dcc.Graph(figure=figures['north'])
+        dcc.RadioItems(
+            id = 'region-selector',
+            options=[
+                {'label': 'All Regions', 'value': 'all'},
+                {'label': 'north', 'value':'north'},
+                {'label': 'south', 'value':'south'},
+                {'label': 'east', 'value':'east'},
+                {'label': 'west', 'value':'west'}
+            ],
+            value='all',
+            inline = True,
+            style={'textAlign': 'center'}
+        )
     ]),
-    html.Div([
-        dcc.Graph(figure=figures['south'])
-    ]),
-    html.Div([
-        dcc.Graph(figure=figures['east'])
-    ]),
-    html.Div([
-        dcc.Graph(figure=figures['west'])
-    ]),
-    html.Div([
-        dcc.Graph(figure = fx)
-    ])
+    dcc.Graph(id = 'sales-graph', style = {'height': '500px'})
 ])
+
+@callback(
+    Output('sales-graph', 'figure'),
+    Input('region-selector', 'value')
+)
+def update_graph(selected_region):
+    if selected_region == 'all':
+        new_df = df.copy()
+        fig = px.line(new_df, x = 'date', y = 'sales', color = 'region', title = 'Sales Trend (All Regions)', labels={'sales': 'Sales ($)', 'date': 'Date', 'region': 'Region'},
+        color_discrete_map = color_dict
+        )
+        fig.update_layout(showlegend=True)
+    else:
+        new_df = df[df['region']==selected_region]
+        fig = px.line(new_df, x = 'date', y = 'sales',title = f'Sales Trend (for {selected_region} Region)',
+        labels = {'sales':'Sales ($)', 'date': 'Date'})
+        fig.update_traces(
+            line_color = color_dict[selected_region],
+            line_width = 3
+        )
+        fig.update_layout(showlegend=False)
+    
+    fig.add_vline(
+        x = breakpoint_date,
+        line_dash = 'dot',
+        line_color = 'red',
+        line_width = 5
+    )
+
+    fig.update_layout(
+        xaxis_title = 'Date',
+        yaxis_title = 'Sales ($)'
+
+    )
+    return fig
+
 
 if __name__ == '__main__':
     app.run(debug=True)
